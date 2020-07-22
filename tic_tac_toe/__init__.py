769ee8e9791122e5ec9f03093029c71a2b0f12ce
MurphysLaw <cngfootball@gmail.com>
2020-07-21 21:10:56 -0600

    Create Game Window with Pygame

--- tic_tac_toe/__init__.py
+++ tic_tac_toe/__init__.py
@@ -1,1 +1,16 @@
 __version__ = '0.1.0'
+import pygame
+
+#Initialize the pygame
+pygame.init()
+
+#create the screen
+screen = pygame.display.set_mode((1200,600))
+
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
