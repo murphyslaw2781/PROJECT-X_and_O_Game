7994e19255b598cfb3bd3c032cd609eea855a2c2
MurphysLaw <cngfootball@gmail.com>
2020-07-24 18:27:56 -0600

    Name input, Select Character, Move Character

--- tic_tac_toe/__init__.py
+++ tic_tac_toe/__init__.py
@@ -8,24 +8,27 @@ mainClock = pygame.time.Clock()
 from pygame.locals import *
 pygame.init()
 pygame.display.set_caption("Axe's and Oh's")
-screen = pygame.display.set_mode((500, 500),0,32)
-font = pygame.font.SysFont('comicsansms', 20)
-
+screen = pygame.display.set_mode((1000, 800),0,32)
+font = pygame.font.SysFont('comicsansms', 35)
+font_color = pygame.Color('white')
+x = 500
+y = 400
 
 # Write on Screen
 def draw_text(text, font, color, surface, x, y):
-    textobj = font.render(text, 1, color)
+    textobj = font.render(text,1, color)
     textrect = textobj.get_rect()
-    textrect.topleft = (x, y)
+    textrect.center = (x, y)
     surface.blit(textobj, textrect)
 
 # Collect user input /Text Box
-def text_box():
-    pass
+def header_message(text):
+    draw_text(text,font, font_color, screen, x,y-200)
     
-
-
 def main_menu():
+    pass
+
+def player():
     player_name = ''
     color_active = pygame.Color('gray15')
     color_passive = pygame.Color('white')
@@ -34,20 +37,18 @@ def main_menu():
     while True:
         #RGB - Red, Blue, Green
         screen.fill((0,0,0))
-        
-        # Menu Buttons
+        header_message('Player Name')
+        #Mouse
         mx, my = pygame.mouse.get_pos()
         #Text Input Box
-        draw_text("Player Name",font, (255,255,255), screen, (100+(150/2)),(100+(50/2)))
-        button_1 = pygame.Rect(150, 175, 200, 50)
+        button_1 = pygame.Rect(x-100, y-50, 200,50)
         pygame.draw.rect(screen,color, button_1)
-        #Text Render Player Name
         text_surface = font.render(player_name,True,(0,0,0))
-        screen.blit(text_surface,(button_1.x+5,button_1.y+15))
+        screen.blit(text_surface,(button_1.x+10,button_1.y))
         #Lets Play!
-        button_3 = pygame.Rect(150, 400, 200, 50)
+        button_3 = pygame.Rect(x-100, y+250, 200, 50)
         pygame.draw.rect(screen, (255, 0, 0), button_3)
-        draw_text("Play",font, (255, 255, 255), screen,225,415)
+        draw_text("Submit",font, (255, 255, 255), screen,x,y+270)
         
         if button_1.collidepoint((mx, my)):
             if click:
@@ -56,7 +57,7 @@ def main_menu():
                 color = color_active
         if button_3.collidepoint((mx, my)):
             if click:
-                pass
+                choose(player_name)
         # Menu Events             
        
         for event in pygame.event.get():
@@ -67,7 +68,7 @@ def main_menu():
                 if event.key == K_BACKSPACE:
                     player_name = player_name[:-1]
                 elif event.key == K_RETURN:
-                    choose()
+                    choose(player_name)
                 else:
                     player_name += event.unicode
             if event.type == MOUSEBUTTONDOWN:
@@ -77,31 +78,52 @@ def main_menu():
         pygame.display.update()
         mainClock.tick(60)
 
-def choose():
+def choose(player_name):
+    
     click = False
     running = True
     while running:
 
         screen.fill((0,0,0))
-        draw_text('Welcome', font, (255, 255, 255), screen, (100+(200/2)),100)
-        draw_text("Choose Your Player", font, (255, 255, 255), screen, (100+(120/2)),(100+(100/2)))
+        header_message('Welcome '+player_name)
+        draw_text("Choose Your Player", font, (255, 255, 255), screen, x,y-50)
         
         mx, my = pygame.mouse.get_pos()
         
-        # Text Box
-        input_box = pygame.Rect(150, 200, 200, 50)
-        pygame.draw.rect(screen, (255, 255, 255), input_box)
-        if input_box.collidepoint((mx,my)):
-            if click:
-                pass
+        # Avatars
+        viking_image_click = pygame.Rect(x-55,y, 200, 200)
+        viking_image = pygame.image.load('viking.png')
+        screen.blit(viking_image,(x-35,y))
 
+        viking_image2_click = pygame.Rect(x-300,y, 200, 200)
+        viking_image2 = pygame.image.load('viking2.png')
+        screen.blit(viking_image2,(x-255,y))
+
+        viking_image3_click = pygame.Rect(x+150,y+10, 100, 200)
+        viking_image3 = pygame.image.load('viking3.png')
+        screen.blit(viking_image3,(x+150,y+10))
+
+        #pygame.draw.rect(screen, (255, 255, 255), input_box)
+        if viking_image_click.collidepoint((mx,my)):
+            if click:
+                character = pygame.image.load('viking.png')
+                game(character)
+        if viking_image2_click.collidepoint((mx,my)):
+            if click:
+                character = pygame.image.load('viking2.png')
+                game(character)
+        if viking_image3_click.collidepoint((mx,my)):
+            if click:
+                character = pygame.image.load('viking3.png')
+                game(character)
         # Menu Buttons
-        button_1 = pygame.Rect(150, 400, 200, 50)
+        button_1 = pygame.Rect(x-100, y+250, 200, 50)
         pygame.draw.rect(screen, (255, 0, 0), button_1)
-        draw_text('Submit', font, (255, 255, 255), screen, 215,415)
+        draw_text('Play', font, (255, 255, 255), screen,x,y+270)
+
         if button_1.collidepoint((mx, my)):
             if click:
-                main_menu()
+                game()
         # Text Input
         
 
@@ -112,7 +134,8 @@ def choose():
                 sys.exit()
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
-                    running = False
+                    pygame.quit()
+                    sys.exit()
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
@@ -121,29 +144,17 @@ def choose():
         pygame.display.update()
         mainClock.tick(60)
 
-def game():
+def game(character):
+    player_x = 485
+    player_y = 700
     running = True
+    click = False
     while running:
 
         screen.fill((0,0,0))
-        draw_text('Welcome Player 2', font, (255, 255, 255), screen, 165,50)
-        mx, my = pygame.mouse.get_pos()
-
-        # Text Box
-        input_box = pygame.Rect(150, 200, 200, 50)
-        pygame.draw.rect(screen, (255, 255, 255), input_box)
-        if input_box.collidepoint((mx,my)):
-            if click:
-                pass
-
-        # Menu Buttons
+        screen.blit(character,(player_x,player_y))
         
-        button_1 = pygame.Rect(150, 400, 200, 50)
-        pygame.draw.rect(screen, (255, 0, 0), button_1)
-        draw_text('Submit', font, (255, 255, 255), screen, 215,415)
-        if button_1.collidepoint((mx, my)):
-            if click:
-                main_menu()
+        mx, my = pygame.mouse.get_pos()
 
         # Menu Events
         click=False
@@ -153,7 +164,18 @@ def game():
                 sys.exit()
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                if event.key == K_BACKSPACE:
                     running = False
+                if event.key == K_LEFT:
+                    player_x=player_x-50
+                if event.key == K_RIGHT:
+                    player_x = player_x+50
+                if event.key == K_UP:
+                    player_y = player_y-20
+                if event.key == K_DOWN:
+                    player_y = player_y+20
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
@@ -163,4 +185,4 @@ def game():
 
 #Game Loop Written Below
 
-main_menu()
+player()
